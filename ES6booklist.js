b6c6689945ea7d664e86064f1e7dd82d56e7c052
sanch0y <35249103+sanch0y@users.noreply.github.com>
2020-10-24 19:41:16 +0600

    New Edit Feature added

--- ES6booklist.js
+++ ES6booklist.js
@@ -21,9 +21,19 @@ class UI {
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.year}</td>
-    <td><a href="#" class="delete">X</a></td>`
+    <td>
+    <button type="button" class="btn btn-warning btn-sm edit">Edit</button>
+    <button type="button" class="btn btn-danger btn-sm delete">Delete</button>
+    </td>
+    `
     //*/
     tbody.appendChild(row);
+  }
+  //____________________________________________________________________________
+  putFields(book) {
+    document.getElementById('title').value = book.title;
+    document.getElementById('author').value = book.author;
+    document.getElementById('publish').value = book.year;
   }
   //____________________________________________________________________________
   clearFields() {
@@ -48,11 +58,50 @@ class UI {
     }, 3000);
   }
   //____________________________________________________________________________
+  editRow(target) {
+    console.log(target);
+    let id = target.parentNode.parentNode.firstElementChild;
+    let title = id.nextElementSibling;
+    let author = title.nextElementSibling;
+    let publishyear = author.nextElementSibling;
+    const book = new Book(id.textContent, title.textContent, author.textContent, publishyear.textContent);
+    ui.putFields(book);
+    let editBtn = document.getElementById('submit');
+    editBtn.value = 'Edit Book Info';
+    let counter = 0;
+    editBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      
+      const books = Store.getBooks();
+      books.forEach((book, index) => {
+        if (book.bookId == id.textContent) {
+          counter++;
+          // console.log('counter',counter);
+          if(counter == 1){
+            book.title = document.getElementById('title').value;
+            book.author = document.getElementById('author').value;
+            book.year = document.getElementById('publish').value;
+            books.splice(index, 1, book);
+
+            title.innerText = document.getElementById('title').value;
+            author.innerText = document.getElementById('author').value;
+            publishyear.innerText = document.getElementById('publish').value;
+
+            ui.clearFields();
+
+            editBtn.value = 'Add New Book';
+          }
+        }
+      });
+
+      localStorage.setItem('books', JSON.stringify(books));
+    });
+    return 'edited';
+  }
+  //____________________________________________________________________________
   deleteRow(target) {
-    if (target.className === 'delete') {
-      target.parentNode.parentNode.remove();
-      return 'deleted';
-    }
+    target.parentNode.parentNode.remove();
+    return 'deleted';
   }
 
 }
@@ -137,26 +186,27 @@ function formListener(evnt) {
 }
 
 const tbody = document.getElementById('book-list');
-tbody.addEventListener('click', deleteEvent);
+tbody.addEventListener('click', editOrDeleteEvent);
 //______________________________________________________________________________
-function deleteEvent(evnt) {
+function editOrDeleteEvent(evnt) {
+  // evnt.stopImmediatePropagation();
   evnt.preventDefault();
-  let status = ui.deleteRow(evnt.target);
-  if (status === 'deleted') {
-    ui.alertMsg('Book Record Deleted', 'success');
+  let status = '';
+  if (evnt.target.classList.contains('edit')) {
+    status = ui.editRow(evnt.target);
+    if (status === 'edited') {
+      ui.alertMsg('Book Record Edited', 'success');
+    }
+  } else if (evnt.target.classList.contains('delete')) {
+    status = ui.deleteRow(evnt.target);
+    if (status === 'deleted') {
+      ui.alertMsg('Book Record Deleted', 'success');
+    }
+    //Delete from localStorage
+    Store.removeBook(evnt.target);
   }
-  //Delete from localStorage
-  Store.removeBook(evnt.target);
 }
+
 
 //Display from localStorage when DOM is loaded
 document.addEventListener('DOMContentLoaded', Store.displayBooks);
-
-
-
-
-
-
-
-
-//console.log(form);
